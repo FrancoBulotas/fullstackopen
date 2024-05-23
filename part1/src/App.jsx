604e51cd43abfc828d16c9074cfb97c5189dfebd
FrancoBulotas <francothiago10@hotmail.com>
2024-05-23 00:55:15 -0300

    up to 1.14 step 3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,7 @@
-
 import { useState } from 'react'
 
-const Header = (props) =>{
-  return (
+const Header = (props) => {
+  return(
     <div>
       <h1>{props.text}</h1>
     </div>
@@ -10,78 +9,44 @@ const Header = (props) =>{
 }
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
-
-const Display = ({text, amount}) => {
-  return (
-    <tbody>
-      <tr>
-        <td>{text}</td>
-        <td>{amount}</td>
-      </tr>
-    </tbody>
-  )
-}
-
-const Statictis = (props) => {
-
-  if(props.total == 0){
-    return (
-      <div>
-        <Header text={'statistics'} />
-        <p>No feedback given</p>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <Header text={'statistics'} />
-      <table>
-        <Display text={'good'} amount={props.good} />
-        <Display text={'neutral'} amount={props.neutral} />
-        <Display text={'bad'} amount={props.bad} />
-        <Display text={'total'} amount={props.total} />
-        <Display text={'positive'} amount={props.positive} />
-      </table>
-    </div>
-  )
-}
+let points = []
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  let positive = [0, ' %'];
-
-  const handleGoodClick = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    setTotal(total + 1)
-  }
-  const handleNeutralClick = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    setTotal(total + 1)
-  } 
-  const handleBadClick = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    setTotal(total + 1)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  if (points.length == 0){
+    points = Array(anecdotes.length).fill(0)
   }
   
-  if(total>0){
-    positive = [good/total, " %"]
+  const [selected, setSelected] = useState(0)
+
+  const handleVoteClick = () =>{
+    points[selected] += 1  
   }
 
+  const handleRandomClick = () => {
+    const updatedSelected = Math.floor(Math.random() * anecdotes.length)
+    setSelected(updatedSelected)    
+  }
+  
   return (
     <div>
-      <Header text={'give feedback'} />
-      <Button handleClick={handleGoodClick} text={'good'} />
-      <Button handleClick={handleNeutralClick} text={'neutral'} />
-      <Button handleClick={handleBadClick} text={'bad'} />
-      <Statictis good={good} neutral={neutral} bad={bad} total={total} positive={positive} />
+      <Header text={'Anecdote of the day'} />
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <Button handleClick={handleVoteClick} text={'vote'} />
+      <Button handleClick={handleRandomClick} text={'next anecdote'} />
+      <Header text={'Anecdote with most votes'} />
+      <div>{anecdotes[points.indexOf(Math.max(...points))]}</div>
+      <div>has {points[points.indexOf(Math.max(...points))]} votes</div>
     </div>
   )
 }
