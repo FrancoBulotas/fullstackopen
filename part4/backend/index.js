7d03fc0b677c423846ce0c5b48adfbd1adfe00c7
FrancoBulotas <francothiago10@hotmail.com>
2024-05-29 14:13:39 -0300

    up to 4.4

---
+++ part4/backend/index.js
@@ -0,0 +1,10 @@
+
+
+const app = require('./app')
+const config = require('./utils/config')
+// const logger = require('./utils/logger')
+
+const PORT = config.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
