7e2a588bf506a599a1ed393772c6332fcfa5ed38
FrancoBulotas <francothiago10@hotmail.com>
2024-06-01 00:10:35 -0300

    up to 4.14 with some edits that in the course are on the note app

--- part4/backend/app.js
+++ part4/backend/app.js
@@ -5,7 +5,8 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blogs')
+const blogRouter = require('./controllers/blogs') 
+const usersRouter = require('./controllers/users') 
 const middleware = require('./utils/middleware')
 // const logger = require('./utils/logger')
 
@@ -16,6 +17,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 app.use(middleware.errorHandler)
 
 
--- part4/backend/tests/test_helper.js
+++ part4/backend/tests/test_helper.js
@@ -1,7 +1,7 @@
 
 
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
 
 const initialBlogs = [
     {
@@ -23,7 +23,13 @@ const blogsInDb = async () => {
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(user => user.toJSON())
+}
+
 module.exports = {
     initialBlogs,
-    blogsInDb
+    blogsInDb,
+    usersInDb
 }
